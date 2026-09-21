d33a10f16911115e0f6ad1bfd2a0793e5c5b05f3
sreevalli2307 <sreevalligangone@gmail.com>
2026-09-21 16:39:03 +0530

    9-21

--- Assignments/M01_Sliding_Window_&_Two_Pointers/AST03/task .py
+++ Assignments/M01_Sliding_Window_&_Two_Pointers/AST03/task .py
@@ -1,16 +1,16 @@
 def countGoodSubstrings(s: str) -> int:
     count = 0
-
+    
+    # Iterate through all possible starting indices for a 3-character substring
     for i in range(len(s) - 2):
-        
         substring = s[i:i + 3]
-
+        # Check if all 3 characters are distinct
         if len(set(substring)) == 3:
             count += 1
 
     return count
 
 
-if __name__ == "_main_":
-    s = input()
+if __name__ == "__main__":
+    s = input().strip()
     print(countGoodSubstrings(s))
