69dca6ad3de9d84df7956a62a4fc3a7d7594c4b6
sreevalli2307 <sreevalligangone@gmail.com>
2026-09-22 09:25:50 +0530

    msg

---
+++ Practice/M04_Linked_list/S01/Singly_Linked_list.py
@@ -0,0 +1,27 @@
+'''DSA: The way of 
+ --> Storing
+ --> Organizing
+ -->Managing of data of efficient usage
+ Application : library --> Secions --> rows --> Books
+ What are the types :
+ 1.Primitive:int,float,str,bool
+ 2.Non-Primitive:
+  1. Linear : Arrays,List,Linked List,Stacks,Queues
+  2.Non - Linear:Trees,Graphs
+Linked List:The elements can be store in the form of nodes and each nodes have the connection
+Node : 2 parts 
+ 1. Data --> the actual value present 
+ 2. Reference --> this will be points to nex node
+Difference between List $ Linked List 
+List                                      | Linked List
+1.Inserting & Deletion are difficult      | 1.Insertion & Deletion are very efficient
+2.Indexing                                | 2.Non Indexing
+3. Easy to traverse                       | 3. Traverse each node
+  
+Types of Linked List 
+1. Singly 
+2.Doubly 
+3.Circular
+1. Singly --> n
+'''
+
